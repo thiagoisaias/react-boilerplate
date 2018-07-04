c687ac278d00a44719c204b4bd34452c5c07a6d1
Thiago <thiagoisaiasbrg@gmail.com>
2018-07-04 00:09:16 -0300

    Move some config files to package json

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  setupTestFrameworkScriptFile: '<rootDir>/enzyme.config.js',
-  snapshotSerializers: ['enzyme-to-json/serializer'],
-  testRegex: 'tests/.*\\.test\\.js$',
-  verbose: true,
-};
