be9d128fdcb4d0006201f5e990aa81e71ff080ca
Thiago <thiagoisaiasbrg@gmail.com>
2018-07-04 00:21:44 -0300

    Move config files to config folder

---
+++ config/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    main: 'app/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, '../build'),
+    filename: '[name].[hash].main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'babel-loader' }, { loader: 'eslint-loader' }]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      root: path.resolve(__dirname, '../'),
+      app: path.resolve(__dirname, '../app'),
+      components: path.resolve(__dirname, '../app/components')
+    }
+  },
+  devServer: {
+    port: 3000
+  },
+  plugins: [
+    new CleanWebpackPlugin('../build', {
+      allowExternal: true
+    }),
+    new HtmlWebPackPlugin({
+      template: 'app/index.html',
+      filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css'
+    })
+  ]
+};
